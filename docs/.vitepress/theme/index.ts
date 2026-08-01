9991dc0c38fb481591df7f2087abcd94fb9f30d2
joinwell52 <joinwell52@gmail.com>
2026-08-01 17:51:08 +0800

    feat(portal-v5): register portal component and styles

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
+import PortalHome from './components/PortalHome.vue'
 import './custom.css'
 import './mobile-fix.css'
 import './rvs.css'
+import './portal-v5.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('PortalHome', PortalHome)
+  }
+} satisfies Theme
