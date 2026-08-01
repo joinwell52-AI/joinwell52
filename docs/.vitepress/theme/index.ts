8e1d91c96f2db19040eef965c12c032d0b62d900
joinwell52 <joinwell52@gmail.com>
2026-08-01 17:52:53 +0800

    feat(portal-v5): register article cover component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,17 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import PortalHome from './components/PortalHome.vue'
+import ArticleCover from './components/ArticleCover.vue'
 import './custom.css'
 import './mobile-fix.css'
 import './rvs.css'
 import './portal-v5.css'
+import './article-cover.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('PortalHome', PortalHome)
+    app.component('ArticleCover', ArticleCover)
   }
 } satisfies Theme
