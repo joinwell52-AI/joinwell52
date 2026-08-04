46af8e4674c796550a755f1308382c06cc9b8897
joinwell52 <joinwell52@gmail.com>
2026-08-04 18:49:05 +0800

    ui: select legacy or V5 operations data by date

---
+++ docs/.vitepress/theme/components/RuntimeOperationsCenterCurrent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import legacyData from '../../generated/runtime-legacy-records.json'
+import RuntimeOperationsCenterClassic from './RuntimeOperationsCenterClassic.vue'
+import RuntimeOperationsCenterLegacy from './RuntimeOperationsCenterLegacy.vue'
+
+type Lang = 'en' | 'zh'
+type LegacyData = { current: unknown | null }
+
+const props = withDefaults(defineProps<{ lang?: Lang }>(), { lang: 'en' })
+const legacy = legacyData as LegacyData
+const useLegacy = computed(() => Boolean(legacy.current))
+</script>
+
+<template>
+  <RuntimeOperationsCenterLegacy v-if="useLegacy" :lang="props.lang" />
+  <RuntimeOperationsCenterClassic v-else :lang="props.lang" />
+</template>
